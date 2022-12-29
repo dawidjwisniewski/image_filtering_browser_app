b53a79e705f870ba25a0f905bca6f2b1cdbf117c
Dawid Wisniewski <dawid.j.wisniewski@gmail.com>
2022-12-29 14:48:17 +0100

    handle passwords better

--- ifbRestApis/settings.py
+++ ifbRestApis/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import datetime
+from my_secrets import secret_string
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,7 +110,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ifb_db',
         'USER': 'django',
-        'PASSWORD': 'ifb_pass',
+        'PASSWORD': secret_string,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
